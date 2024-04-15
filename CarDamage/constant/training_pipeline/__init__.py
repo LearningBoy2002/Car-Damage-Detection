5c2a41bb3acf55abb1d1164d6012ceb2539b23ad
LearningBoy2002 <bishwarupbbiswas@gmail.com>
2024-04-16 03:26:00 +0530

    First commit

---
+++ CarDamage/constant/training_pipeline/__init__.py
@@ -0,0 +1,24 @@
+ARTIFACTS_DIR: str = "artifacts"
+
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/drive/u/0/folders/1sThTSKsMjRKc-IgJ3fagTeNx4E6a9d3m"
+
+
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["test","train", "valid", "data.yaml"]
+
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
+
+MODEL_TRAINER_NO_EPOCHS: int = 100
+
+MODEL_TRAINER_BATCH_SIZE: int = 16
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'CarDamage',
+    version= '0.0.0',
+    author= 'LearningBoy2002',
+    author_email= 'bishwarupbbiswas@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
+
